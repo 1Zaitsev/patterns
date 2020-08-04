cb67c0cdb0287cfee528d1ec3ad1440e21ccf346
1Zaytsev <4yu@bk.ru>
2020-08-04 16:53:27 +0300

    Data mapper

---
+++ patterns_lesson6/src/test/java/qdo_ln/patterns_lesson6/PatternsLesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package qdo_ln.patterns_lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsLesson6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
